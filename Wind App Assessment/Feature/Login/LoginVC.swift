1d9926d13b82e9d412c1bef3e4eb047d6f05d0ac
MD RUBEL <hello@mdrubel.net>
2023-03-08 00:07:32 +0600

    project setup

---
+++ Wind App Assessment/Feature/Login/LoginVC.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVC.swift
+//  Wind App Assessment
+//
+//  Created by MD RUBEL on 3/8/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
