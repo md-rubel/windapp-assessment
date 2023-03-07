1f859ac9c2f63641a7566ac9b3dc180bce431c39
MD RUBEL <hello@mdrubel.net>
2023-03-07 23:51:30 +0600

    Initial Commit

---
+++ Wind App Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wind App Assessment
+//
+//  Created by MD RUBEL on 3/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
